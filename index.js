a2d13c2997b7f1c00298410b08fc0498c9969c7d
tithi2149 <tanyasultana60@gmail.com>
2023-11-06 01:45:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const express = require('express')
+const cors=require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.3vwvgpx.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+        const database = client.db("Allblogs");
+        const AllblogsCollection = database.collection("allblog");
+
+        app.get('/allblogs', async(req, res) => {
+            const cursor=AllblogsCollection.find().sort({ published_date: -1 })
+            const result = await cursor.toArray()
+            res.send(result) 
+              
+          });
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
